7f4dd308c1813e3c54d98f6ac27983ad95ee6c3d
Sangenis Pablo <sangenispablo@gmail.com>
2024-07-06 09:17:33 -0300

    Trabajando con el progress

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: ``
+  styles: ``,
 })
 export class ProgressComponent {
+  @Input()
+  progreso: number = 50;
 
+  get getPorcentaje() {
+    return `${this.progreso}%`;
+  }
+
+  cambiarValor(valor: number) {
+    if (this.progreso >= 100 && valor > 0) {
+      return (this.progreso = 100);
+    }
+    if (this.progreso <= 0 && valor < 0) {
+      return (this.progreso = 0);
+    }
+    return this.progreso = this.progreso + valor;
+  }
 }
